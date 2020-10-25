8494c1ae9e64250f67c9c3a24073a2470b04ce4f
ryan-li20 <rli20@stuy.edu>
2020-10-24 22:10:59 -0400

    finish, will update after tester finished

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 public class ArrayOps{
+
   public static int sum(int[] arr){
     int sum = 0;
     for(int i = 0; i < arr.length; i++){
@@ -7,6 +8,7 @@ public class ArrayOps{
     }
     return sum;
   }
+  // sum() takes an array of int, and returns a sum of the individual values in the array.
 
   public static int largest(int[] arr){
     int highest = arr[0];
@@ -17,6 +19,7 @@ public class ArrayOps{
     }
     return highest;
   }
+  // largest() takes a non-empty array of int, and returns the largest element of the array.
 
   public static int[] sumRows(int[][] arr){
     int[] summing = new int[arr.length];
@@ -25,6 +28,8 @@ public class ArrayOps{
     }
     return summing;
   }
+  /* sumRows() takes a 2D array of int, and returns a 1D array of int with each element being
+  the sum of the individual rows of the 2D array. */
 
   public static int[] largestInRows(int[][] arr){
     int[] largest = new int[arr.length];
@@ -33,6 +38,8 @@ public class ArrayOps{
     }
     return largest;
   }
+  /* largestInRows() takes a nonempty 2D array of int, the inner arrays cannot be empty either,
+   and returns a 1D array of int with each element being the largest element of the individual rows  of the 2D array. */
 
   public static int sum(int[][] arr){
     int sum = 0;
@@ -41,6 +48,7 @@ public class ArrayOps{
     }
     return sum;
   }
+  // sum() takes a 2D array of int, and returns the sum of all the individual values in all of the arrays.
 
   public static int[] sumCols(int[][] arr){
     int[] sums = new int[arr[0].length];
@@ -51,6 +59,8 @@ public class ArrayOps{
     }
     return sums;
   }
+  /* sumCols takes a 2D array that must be rectangular (all rows have the same length) and returns a 1D array of int
+  with each element being the sum of the individual columns of the 2D array. */
 
   public static boolean isRowMagic(int[][] arr){
     int sum = ArrayOps.sum(arr[0]);
@@ -62,6 +72,7 @@ public class ArrayOps{
     return true;
 
   }
+  // isRowMagic() takes a rectangular 2d array and returns true when each row has the same sum, and false otherwise.
 
   public static boolean isColMagic(int[][] arr){
     int[] newSums = ArrayOps.sumCols(arr);
@@ -73,4 +84,10 @@ public class ArrayOps{
     }
     return true;
   }
+  // isColMagic() takes a rectangular 2d array and  returns true when each column has the same sum, and false otherwise.
+
+  public static boolean isLocationMagic(int[][] arr, int row, int col){
+    return ArrayOps.sum(arr[row]) == ArrayOps.sumCols(arr)[col];
+  }
+  // isLocationMagic() takes a rectangular 2d array and returns true when the row and column specified have the same sum.
 }
